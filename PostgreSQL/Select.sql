fc4680fd6a62870c3873fceff0e03516a86e7280
Oleksandr Minchuk <aminchuk.edu@gmail.com>
2022-08-09 15:57:24 +0300

    Add Select queries

---
+++ PostgreSQL/Select.sql
@@ -0,0 +1,36 @@
+--1) Display all films presented in DB
+SELECT * 
+FROM film;
+--2) Display film's title, its release year and ordered by the title(Ascending order)
+SELECT title, release_year 
+FROM film 
+ORDER BY 1;
+--3) Making columns more descriptive
+SELECT title AS movie_title, release_year AS date_of_release
+FROM film f 
+ORDER BY 2,1;
+--4) Display possible movie rating
+SELECT DISTINCT rating
+FROM film
+ORDER BY 1 DESC;
+--5) Display actors avaliable in DB, ordered by first name and last name
+SELECT actor_id, first_name, last_name
+FROM actor a 
+ORDER BY 2,3;
+--6) Conecting first name to last name to create full name column 
+SELECT actor_id, first_name || ' ' || last_name AS actor_full_name
+FROM actor a 
+ORDER BY 2;
+--7) Finding actor with the name Cruise
+SELECT actor_id, first_name || ' ' || last_name AS actor_full_name
+FROM actor
+WHERE upper(last_name) = 'CRUISE';
+--8)Finding actor with 'BAR' in name
+SELECT actor_id, first_name || ' ' || last_name AS actor_full_name
+FROM actor
+WHERE upper(last_name) LIKE ('BAR%');
+--9)Finding actors with name 'CRUISE', 'BARRYMORE' and 'HOPKINS'
+SELECT actor_id, first_name || ' ' || last_name AS actor_full_name
+FROM actor
+WHERE upper(last_name) IN ('CRUISE', 'BARRYMORE', 'HOPKINS')
+ORDER BY 2;
